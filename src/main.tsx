4cbd811347c60cb32797ce53d2f24eaf13b528b6
rameshsapkota900 <rameshsapkota900@gmail.com>
2026-05-08 21:46:22 +0545

    Change BrowserRouter to HashRouter for GitHub Pages compatability

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from '@/App'
 import { AuthProvider } from '@/lib/auth-context'
 import { NotificationProvider } from '@/lib/notification-context'
@@ -9,12 +9,7 @@ import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
+    <HashRouter>
       <AuthProvider>
         <NotificationProvider>
           <App />
@@ -32,6 +27,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           />
         </NotificationProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
